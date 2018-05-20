bf30018d15d2144cfdeed7237d12f48e95b6c822
perdomo24 <tito.007@hotmail.es>
2018-05-20 14:31:15 -0600

    Cambio de variables

--- Movie/PHP/Database/conexion.php
+++ Movie/PHP/Database/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $server = "moviesutec-mysqldbserver.mysql.database.azure.com";//servidor
 $user = "MoviesUtec@moviesutec-mysqldbserver";//usuario
-$password = "abc123...";//poner tu propia contraseña, si tienes una.
+$password = "abc123..";//poner tu propia contraseña, si tienes una.
 $bd = "mysqldatabase29190";//base de dato
 	//funcion que verifica la conexion
 	$connect = mysqli_connect($server, $user, $password, $bd);
