7d1277e5c2d09e0bdf213bb2a58e26f43411be13
perdomo24 <tito.007@hotmail.es>
2018-05-20 16:30:35 -0600

    Paginas Mision y Vision

---
+++ Movie/PHP/Informacion/Mision.php
@@ -0,0 +1,77 @@
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Principal</title>
+    <link href="..\..\Vendor/bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="..\..\CSS/carousel.css" rel="stylesheet">
+    <link href="..\..\Vendor\alertify\css\alertify.min.css" rel="stylesheet">
+    <link href="..\..\Vendor\alertify\css\alertify.rtl.min.css" rel="stylesheet">
+    <link href="..\..\Vendor\alertify\css\themes\default.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Peliculas Utec</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="../../Index.html">Inicio</a></li>
+                <li><a href="..\..\PHP\Registro\Registro.php">Registrate</a></li>
+                <li><a href="..\..\PHP\Registro\Login.php">Inicia Sesion</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Valores <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                      <li><a href="Mision.php">Misión</a></li>
+                      <li><a href="Vision.php">Visión</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+
+
+    <div class="col-lg-12" style="margin-top:150px">
+      <div class="container">          
+                <div class="container-fluid">                
+                    <div class="col-sm-8">
+                    <h2>Nuestros Valores</h2><br>
+                    <h4><strong>Misión:</strong> Ofrecer un Sitio Web de música y películas, a través de un servicio de alta calidad y confiable, con diseño de vanguardia, y lo más nuevo de ámbito cinematográfico y musical.</h4><br>
+                </div>          
+              
+          </div>
+      </div>
+      <footer>
+
+        <p >© 2018 Utec</p>
+      </footer>
+
+    </div>
+    <script src="..\..\Vendor\Jquery\jquery-3.3.1.min.js"></script>
+    <script src="..\..\Vendor\bootstrap-3.3.7\js\bootstrap.min.js"></script>
+      <script src="..\..\Vendor\alertify\alertify.min.js"></script>
+
+</body>
+</html>
+<script>
+
+
+
+</script>
