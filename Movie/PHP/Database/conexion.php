78236a105537032b56e421aff778337dc984b77c
perdomo24 <tito.007@hotmail.es>
2018-05-20 14:53:45 -0600

    prueba subir cambios

--- Movie/PHP/Database/conexion.php
+++ Movie/PHP/Database/conexion.php
@@ -5,9 +5,9 @@ $password = "abc123..";//poner tu propia contraseña, si tienes una.
 $bd = "mysqldatabase29190";//base de dato
 	//funcion que verifica la conexion
 	$connect = mysqli_init();
-	mysqli_real_connect($conn, $server, $username, $user, $bd, 3306);
+	mysqli_real_connect($connect, $server, $username, $user, $bd, 3306);
 	/*$connect = mysqli_connect($server, $user, $password, $bd);*/
-	if (!$connect){
+	if (mysqli_connect_errno($connect)){
 		die('Error de Conexión: ' . mysqli_connect_errno());
 	}
 ?>
