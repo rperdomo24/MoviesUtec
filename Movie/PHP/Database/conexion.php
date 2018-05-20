f9a378d6a802c5358d7f43c41c585fad52969118
perdomo24 <tito.007@hotmail.es>
2018-05-20 14:47:23 -0600

    c

--- Movie/PHP/Database/conexion.php
+++ Movie/PHP/Database/conexion.php
@@ -4,7 +4,9 @@ $user = "MoviesUtec@moviesutec-mysqldbserver";//usuario
 $password = "abc123..";//poner tu propia contraseña, si tienes una.
 $bd = "mysqldatabase29190";//base de dato
 	//funcion que verifica la conexion
-	$connect = mysqli_connect($server, $user, $password, $bd);
+	$connect = mysqli_init();
+	mysqli_real_connect($conn, $server, $username, $user, $bd, 3306);
+	/*$connect = mysqli_connect($server, $user, $password, $bd);*/
 	if (!$connect){
 		die('Error de Conexión: ' . mysqli_connect_errno());
 	}
