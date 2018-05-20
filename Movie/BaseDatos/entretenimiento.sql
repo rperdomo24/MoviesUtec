ff9f6685f62b21f5f27dccf4451257ae7747429a
Mercally <josuemercally@gmail.com>
2018-05-20 00:53:48 -0600

    Cambios en base de datos y principal de peliculas

--- Movie/BaseDatos/entretenimiento.sql
+++ Movie/BaseDatos/entretenimiento.sql
@@ -7,10 +7,10 @@
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.2.2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = '+00:00';
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,23 +28,23 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `peliculas`
 --
 
-CREATE TABLE `peliculas` (
-  `IdPelicula` int(11) NOT NULL,
-  `Nombre_Pelicula` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `Portada` varchar(500) COLLATE utf8_spanish_ci NOT NULL,
-  `Sipnosis` varchar(2000) COLLATE utf8_spanish_ci NOT NULL,
-  `PaginaOficial` varchar(2000) COLLATE utf8_spanish_ci NOT NULL,
-  `Titulo_Original` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `Genero` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `Nacionalidad` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `duracion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `annio` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
-  `director` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `actores` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `clasificacion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `UrlPelicula` varchar(2000) COLLATE utf8_spanish_ci NOT NULL,
-  `UrlTrailer` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+-- CREATE TABLE `peliculas` (
+--  `IdPelicula` int(11) NOT NULL,
+--  `Nombre_Pelicula` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+--  `Portada` varchar(500) COLLATE utf8_spanish_ci NOT NULL,
+--  `Sipnosis` varchar(2000) COLLATE utf8_spanish_ci NOT NULL,
+--  `PaginaOficial` varchar(2000) COLLATE utf8_spanish_ci NOT NULL,
+--  `Titulo_Original` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+--  `Genero` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+--  `Nacionalidad` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+--  `duracion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+--  `annio` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+--  `director` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+--  `actores` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+--  `clasificacion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+--  `UrlPelicula` varchar(2000) COLLATE utf8_spanish_ci NOT NULL,
+--  `UrlTrailer` int(11) NOT NULL
+--) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -90,8 +90,8 @@ INSERT INTO `usuarios` (`IdUsuario`, `Usuario`, `CorreoElectronico`, `Contrasenn
 --
 -- Indices de la tabla `peliculas`
 --
-ALTER TABLE `peliculas`
-  ADD PRIMARY KEY (`IdPelicula`);
+--ALTER TABLE `peliculas`
+--  ADD PRIMARY KEY (`IdPelicula`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -106,8 +106,8 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
-ALTER TABLE `peliculas`
-  MODIFY `IdPelicula` int(11) NOT NULL AUTO_INCREMENT;
+--ALTER TABLE `peliculas`
+  --MODIFY `IdPelicula` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -119,3 +119,258 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- 
+
+CREATE TABLE ClasificacionPelicula
+(
+  IdClasificacionPelicula int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  Nombre varchar(100),
+  Descripcion varchar(500)
+);
+
+INSERT INTO ClasificacionPelicula(Nombre, Descripcion)
+VALUES('G', 'Todo espectador.'),
+('PG', 'Menores acompañados de sus padres.'),
+('14A', 'Menores de 14 años acompañados por adultos.'),
+('18A', 'Menores de 18 años acompañados por adultos.'),
+('R', 'Restringido, ningún menor de 18 años puede ver esta película.'),
+('A', 'Mayores de 18 años.');
+
+CREATE TABLE GeneroPelicula
+(
+  IdGeneroPelicula int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  Nombre varchar(100),
+  Descripcion varchar(500)
+);
+
+INSERT INTO GeneroPelicula(Nombre, Descripcion)
+VALUES('Drama', 'Descripción...'),
+('Comedia', 'Descripción...'),
+('Acción', 'Descripción...'),
+('Cine ficción', 'Descripción...'),
+('Fantasía', 'Descripción...'),
+('Romance', 'Descripción...'),
+('Musical', 'Descripción...'),
+('Terror', 'Descripción...');
+
+CREATE TABLE Nacionalidades
+(
+  IdNacionalidad int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  Nombre varchar(100),
+  Descripcion varchar(500)
+);
+
+
+
+INSERT INTO Nacionalidades(Nombre, Descripcion)
+VALUES('Venezolana','Venezuela'),
+('Afgana','Afganistán'),
+('Albanesa','Albania'),
+('Alemana','Alemania'),
+('Alto volteña','Alto volta'),
+('Andorrana','Andorra'),
+('Angoleña','Angola'),
+('Argelina','Argelia'),
+('Argentina','Argentina'),
+('Australiana','Australia'),
+('Austriaca','Austria'),
+('Bahamesa','Bahamas'),
+('Bahreina','Bahrein'),
+('Bangladesha','Bangladesh'),
+('Barbadesa','Barbados'),
+('Belga','Belgica'),
+('Beliceña','Belice'),
+('Bermudesa','Bermudas'),
+('Birmana','Birmania'),
+('Boliviana','Bolivia'),
+('Botswanesa','Botswana'),
+('Brasileña','Brasil'),
+('Bulgara','Bulgaria'),
+('Burundesa','Burundi'),
+('Butana','Butan'),
+('Camboyana','Khemer Rep de Camboya '),
+('Camerunesa','Camerun'),
+('Canadiense','Canada'),
+('Centroafricana','Rep Centroafricana'),
+('Chadeña','Chad'),
+('Checoslovaca','Rep. Checa'),
+('Chilena','Chile'),
+('China','China'),
+('China','Taiwan'),
+('Chipriota','Chipre'),
+('Colombiana','Colombia'),
+('Congoleña','Congo'),
+('Costarricense','Costa Rica'),
+('Cubana','Cuba'),
+('Dahoneya','Dahoney'),
+('Danes','Dinamarca'),
+('Dominicana','Rep. Dominicana'),
+('Ecuatoriana','Ecuador'),
+('Egipcia','Egipto'),
+('Emirata','Emiratos Arabes Udo.'),
+('Escosesa','Escocia'),
+('Eslovaca','Rep. Eslovaca'),
+('Española','España'),
+('Estona','Estonia'),
+('Etiope','Etiopia'),
+('Fijena','Fiji'),
+('Filipina','Filipinas'),
+('Finlandesa','Finlandia'),
+('Francesa','Francia'),
+('Gabiana','Gambia'),
+('Gabona','Gabon'),
+('Galesa','Gales'),
+('Ghanesa','Ghana'),
+('Granadeña','Granada'),
+('Griega','Grecia'),
+('Guatemalteca','Guatemala'),
+('Guinesa Ecuatoriana','Guinea Ecuatorial'),
+('Guinesa','Guinea'),
+('Guyanesa','Guyana'),
+('Haitiana','Haiti'),
+('Holandesa','Holanda'),
+('Hondureña','Honduras'),
+('Hungara','Hungria'),
+('India','India'),
+('Indonesa','Indonesia'),
+('Inglesa','Inglaterra'),
+('Iraki','Irak'),
+('Irani','Iran'),
+('Irlandesa','Irlanda'),
+('Islandesa','Islandia'),
+('Israeli','Israel'),
+('Italiana','Italia'),
+('Jamaiquina','Jamaica'),
+('Japonesa','Japon'),
+('Jordana','Jordania'),
+('Katensa','Katar'),
+('Keniana','Kenia'),
+('Kuwaiti','Kwait'),
+('Laosiana','Laos'),
+('Leonesa','Sierra leona'),
+('Lesothensa','Lesotho'),
+('Letonesa','Letonia'),
+('Libanesa','Libano'),
+('Liberiana','Liberia'),
+('Libeña','Libia'),
+('Liechtenstein','Liechtenstein'),
+('Lituana','Lituania'),
+('Luxemburgo','Luxemburgo'),
+('Madagascar','Madagascar'),
+('Malaca','Fede. de Malasia'),
+('Malawi','Malawi'),
+('Maldivas','Maldivas'),
+('Mali','Mali'),
+('Maltesa','Malta'),
+('Marfilesa','Costa de Marfil'),
+('Marroqui','Marruecos'),
+('Mauricio','Mauricio'),
+('Mauritana','Mauritania'),
+('Mexicana','México'),
+('Monaco','Monaco'),
+('Mongolesa','Mongolia'),
+('Nauru','Nauru'),
+('Neozelandesa','Nueva Zelandia'),
+('Nepalesa','Nepal'),
+('Nicaraguense','Nicaragua'),
+('Nigerana','Niger'),
+('Nigeriana','Nigeria'),
+('Norcoreana','Corea del Norte'),
+('Norirlandesa','Irlanda del norte'),
+('Norteamericana','Estados unidos'),
+('Noruega','Noruega'),
+('Omana','Oman'),
+('Pakistani','Pakistan'),
+('Panameña','Panama'),
+('Paraguaya','Paraguay'),
+('Peruana','Peru'),
+('Polaca','Polonia'),
+('Portoriqueña','Puerto Rico'),
+('Portuguesa','Portugal'),
+('Rhodesiana','Rhodesia'),
+('Ruanda','Ruanda'),
+('Rumana','Rumania'),
+('Rusa','Russia'),
+('Salvadoreña','El Salvador'),
+('Samoa Occidental','Samoa Occidental'),
+('San marino','San Marino'),
+('Saudi','Arabia Saudita'),
+('Senegalesa','Senegal'),
+('Sikkim','Sikkim'),
+('Singapur','Singapur'),
+('Siria','Siria'),
+('Somalia','Somalia'),
+('Sovietica','Union Sovietica'),
+('Sri Lanka','Sri Lanka (Ceylan) '),
+('Suazilandesa','Suazilandia'),
+('Sudafricana','Sudafrica'),
+('Sudanesa','Sudan'),
+('Sueca','Suecia'),
+('Suiza','Suiza'),
+('Surcoreana','Corea del Sur'),
+('Tailandesa','Tailandia'),
+('Tanzana','Tanzania'),
+('Tonga','Tonga'),
+('Tongo','Tongo'),
+('Trinidad y Tobago','Trinidad y Tobago'),
+('Tunecina','Tunez'),
+('Turca','Turquia'),
+('Ugandesa','Uganda'),
+('Uruguaya','Uruguay'),
+('Vaticano','Vaticano'),
+('Vietnamita','Vietnam'),
+('Yemen Rep Arabe','Yemen Rep. Arabe'),
+('Yemen Rep Dem','Yemen Rep. Dem'),
+('Yugoslava','Yugoslavia'),
+('Zaire','Zaire');
+
+CREATE TABLE Peliculas (
+  IdPelicula int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  Titulo varchar(100) NOT NULL,
+  Sinopsis varchar(2000),
+  UrlPaginaOficial varchar(2000),
+  FKGenero int not null REFERENCES GeneroPeliculas(IdGeneroPelicula),
+  FKNacionalidad int not null REFERENCES Nacionalidades(IdGeneroPelicula),
+  DuracionMinutos int,
+  Anio int,
+  Directores varchar(500) DEFAULT '',
+  Actores varchar(500) DEFAULT '',
+  FKClasificacion int not null REFERENCES ClasificacionPelicula(IdClasificacionPelicula),
+  UrlImgPortada varchar(1000),
+  UrlPelicula varchar(1000)
+);
+
+INSERT INTO Peliculas(
+  Titulo, 
+  Sinopsis, 
+  UrlPaginaOficial, 
+  FKGenero, 
+  FKNacionalidad, 
+  DuracionMinutos, 
+  Anio, 
+  Directores, 
+  Actores, 
+  FKClasificacion, 
+  UrlImgPortada, 
+  UrlPelicula)
+VALUES('Titanic', 
+'La trama, una epopeya romántica, relata la relación de Jack Dawson y Rose DeWitt Bukater, dos jóvenes que se conocen y se enamoran a bordo del transatlántico RMS Titanic en su viaje inaugural desde Southampton, Inglaterra, a Nueva York, EE. UU., en abril de 1912. Pertenecientes a diferentes clases sociales, intentan salir adelante pese a las adversidades que los separarían de forma definitiva, entre ellas el prometido de Rose, Caledon «Cal» Hockley (un adinerado del cual ella no está enamorada, pero su madre la ha obligado a permanecer con él para garantizar un futuro económico próspero) y el hundimiento del lujoso barco tras chocar con un iceberg.',
+'https://es.wikipedia.org/wiki/Titanic_(película_de_1997)',
+6, -- Romance
+115,
+195,
+1997,
+'James Cameron',
+'Leonardo DiCaprio, Kate Winslet, Kathy Bates, Frances Fisher, Victor Garber, Bernard Hill...',
+4,
+'https://static.diariofemenino.com/pictures/fotos/194000/194305-peliculas-romanticas-titanic.jpg',
+'blob:https://www.youtube.com/0aecb215-e344-46b3-aebe-21c8a4eff3e3'
+);
+
+CREATE TABLE Trailers
+(
+  IdTrailer int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  FKPelicula int not null REFERENCES Peliculas(IdPelicula),
+  UrlTriller varchar(1000)
+);
--- Movie/PHP/ClientesGratis/Principal.php
+++ Movie/PHP/ClientesGratis/Principal.php
@@ -1,11 +1,52 @@
 <?php
+include ("../Database/conexion.php");
+
 SESSION_START();
+
 	if(!isset($_SESSION['Usuario'])) {
 		header("Location: ..\Registro\Login.php");
 	}
 	else {
     $nom = $_SESSION['Usuario'];
-	?>
+  }
+
+  $queryPeliculas = 
+          "SELECT Titulo, " .
+          "Sinopsis, " .
+          "UrlPaginaOficial, " .
+          "gp.Nombre AS Genero, " .
+          "gp.Descripcion AS GeneroDescripcion, " .
+          "na.Nombre AS Nacionalidad, " .
+          "DuracionMinutos, " .
+          "Anio, " .
+          "Directores, " .
+          "Actores, " .
+          "cl.Nombre AS Clasificacion, " .
+          "cl.Descripcion AS ClasificacionDescripcion, " .
+          "UrlImgPortada, " .
+          "UrlPelicula " .
+          "FROM peliculas AS pe " .
+          "INNER JOIN generopelicula AS gp on gp.IdGeneroPelicula = pe.FKGenero " .
+          "INNER JOIN nacionalidades AS na on na.IdNacionalidad = pe.FKNacionalidad " .
+          "INNER JOIN clasificacionpelicula AS cl on cl.IdClasificacionPelicula = pe.FKClasificacion " .
+          "ORDER BY pe.Anio DESC;";
+
+  $result = getSQLResultSet($connect, $queryPeliculas);
+  
+  function getSQLResultSet($connect, $commando){
+    $mysqli = $connect;
+    if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      printf("Error: %s\n", $mysqli->connect_errno);
+      exit();
+    } else {
+      $rq = $mysqli->query($commando);
+      return $rq;
+    }
+    $mysqli->close();
+  }
+?>
+
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -15,7 +56,6 @@ SESSION_START();
     <link href="../../CSS/Slider.css" rel="stylesheet">
     <link href="..\..\Vendor/bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="..\..\CSS/carousel.css" rel="stylesheet">
-
     <link href="..\..\Vendor\alertify\css\alertify.min.css" rel="stylesheet">
     <link href="..\..\Vendor\alertify\css\alertify.rtl.min.css" rel="stylesheet">
     <link href="..\..\Vendor\alertify\css\themes\default.min.css" rel="stylesheet">
@@ -36,8 +76,8 @@ SESSION_START();
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-
                 <li><a href="#">Inicio</a></li>
+                <li><a href="#">Haste Pro!</a></li>
                 <li><a href="..\..\PHP\Registro\Login.php">Cerrar Sesion</a></li>
               </ul>
             </div>
@@ -46,15 +86,42 @@ SESSION_START();
       </div>
     </div>
     <div class="col-lg-12" style="margin-top:100px">
-
-
       <div class="container">
-        <h5>Bienvenido <?php echo $nom;?> puedes escoger el trailer que desees ver nuestra </h5>
+      <h5>Hola! <b><?php echo $nom;?><b> ¿Qué deseas ver hoy?</h5>
 
+  <?php 
+    while($row = mysqli_fetch_row($result)) {
+      echo '<div class="row__inner">';
+      echo '<div class="tile__media">';
+      echo '<img class="tile__img" src="' . $row[12] . '"/>';
+      echo '</div>';
+      echo '<div class="tile">' . $row[0] . ' - ' . $row[7] . '</div>';
+      echo '<div class="tile__details">Sinopsis: ' . $row[1] . '</div>' ;
+      echo '</div>';
+/*
+      $return[$i] = array("Titulo"=>$row[0],
+        "Sinopsis"=>$row[1],
+        "UrlPaginaOficial"=>$row[2],
+        "Genero"=>$row[3],
+        "GeneroDescripcion"=>$row[4],
+        "Nacionalidad"=>$row[5],
+        "DuracionMinutos"=>$row[6],
+        "Anio"=>$row[7],
+        "Directores"=>$row[8],
+        "Actores"=>$row[9],
+        "Clasificacion"=>$row[10],
+        "ClasificacionDescripcion"=>$row[11],
+        "UrlImgPortada"=>$row[12],
+        "UrlPelicula"=>$row[13]
+      );
+      */
+    }
+  ?>
+
+
+      <!--
 <div class="contain">
-
 <h1>Categorias de Peliculas<small> usuario gratuito</small> </h1>
-
 <div class="row">
   <h1>Romanticas</h1>
 <div class="row__inner">
@@ -109,24 +176,19 @@ Top Gear
 </div>
 
 </div>
-</div>
+</div>-->
 
-</div>
       </div>
-      <footer>
-        <p >© 2018 Utec</p>
-      </footer>
-
     </div>
-    <script src="..\..\Vendor\Jquery\jquery-3.3.1.min.js"></script>
-    <script src="..\..\Vendor\bootstrap-3.3.7\js\bootstrap.min.js"></script>
-    <script src="..\..\Vendor\alertify\alertify.min.js"></script>
-
-</body>
-</html>
+  <footer>
+    <p>© 2018 Utec</p>
+  </footer>
+</div>
+<script src="..\..\Vendor\Jquery\jquery-3.3.1.min.js"></script>
+<script src="..\..\Vendor\bootstrap-3.3.7\js\bootstrap.min.js"></script>
+<script src="..\..\Vendor\alertify\alertify.min.js"></script>
 <script>
 
-
-
 </script>
-<?php } ?>
+</body>
+</html>
